dd8972e869da9dc5ef5698429f7e1708b4d6e146
Kenneth Yuan <kenny.k.yuan@gmail.com>
2018-04-19 11:59:49 +0200

    Created a dynamic endpoint that queries PokeAPI. Also created a new template to display the returned data.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
+@app.route('/pokemon/<var>', methods=['GET'])
+def pokemon(var):
+    r = requests.get('https://www.pokeapi.co/api/v2/pokemon/' + var).json()
+    return render_template('pokemon.html', name=r['name'], id=str(r['id']))
 
 
 if __name__ == '__main__':
